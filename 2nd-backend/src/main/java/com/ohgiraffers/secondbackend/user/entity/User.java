66234931834863dabbb13eceef4d1256d96b8602
이용우 <qhtm008@naver.com>
2025-12-05 10:28:14 +0900

    feat/#61/회원관리 엔티티 생성

---
+++ 2nd-backend/src/main/java/com/ohgiraffers/secondbackend/user/entity/User.java
@@ -0,0 +1,34 @@
+package com.ohgiraffers.secondbackend.user.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+
+
+}
