850b600b95a864dcb0689e19fda6f8953e3fa061
whatimeisitnow <jayjay.hi.77@gmail.com>
2025-12-07 19:36:56 +0900

    feat/71/bookReport-entity

--- 2nd-backend/src/main/java/com/ohgiraffers/secondbackend/bookreport/entity/BookReport.java
+++ 2nd-backend/src/main/java/com/ohgiraffers/secondbackend/bookreport/entity/BookReport.java
@@ -1,26 +1,55 @@
 package com.ohgiraffers.secondbackend.bookreport.entity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
+@Getter
+@NoArgsConstructor
 @Entity
+@Table(name = "book_report")
 public class BookReport {
     @Id
+    @Column(name = "book_report_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long bookReportId;
+    private Long bookReportId;      //독후감 id(pk)
 
-    private Long bookId;
+    @Column(name = "book_id")
+    private Long bookId;        //도서 id(fk)
 
-    private Long userId;
+    @Column(name = "writer_id")
+    private Long writerId;      //글 작성자 id(fk)
 
-    private String bookTitle;
+    @Column(name = "title")
+    private String title;   //제목
 
-    private String description;
+    @Column(name = "description")
+    private String description;     //내용
 
+    @CreationTimestamp
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
 
+    @UpdateTimestamp
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
+
+    @Builder
+    public BookReport(Long bookId, Long writerId, String title, String description){
+        this.bookId = bookId;
+        this.writerId = writerId;
+        this.title = title;
+        this.description = description;
+    }
+
+    // setter대체용
+    public void update(String title, String description){
+        this.title = title;
+        this.description = description;
+        this.updatedAt = LocalDateTime.now();
+    }
 }
