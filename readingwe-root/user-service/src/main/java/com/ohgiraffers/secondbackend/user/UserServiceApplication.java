030e0a9a8285cf688a59a26ac929248ff3c48f8c
이용우 <qhtm008@naver.com>
2025-12-09 04:18:12 +0900

    fix/user-service

--- readingwe-root/user-service/src/main/java/com/ohgiraffers/secondbackend/user/UserServiceApplication.java
+++ readingwe-root/user-service/src/main/java/com/ohgiraffers/secondbackend/user/UserServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
+
 @EnableFeignClients(basePackageClasses = BookFeignClient.class)
 @SpringBootApplication
 public class UserServiceApplication {
