559548dd0e9eef90576a0987c16bec893b8c7fd6
NoSa1e <cjsp1217@gmail.com>
2025-12-09 17:40:27 +0900

    server
    server

--- readingwe-root/user-service/src/main/java/com/ohgiraffers/secondbackend/user/client/EmailFeignClient.java
+++ readingwe-root/user-service/src/main/java/com/ohgiraffers/secondbackend/user/client/EmailFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient("/internal/mail")
+@FeignClient("mail-service")
 public interface EmailFeignClient {
 
     @PostMapping("internal/mail/signup/verification")
