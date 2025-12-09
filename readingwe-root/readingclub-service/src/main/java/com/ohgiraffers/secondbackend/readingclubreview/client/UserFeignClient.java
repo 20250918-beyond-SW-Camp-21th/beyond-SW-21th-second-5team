8c1f1017a01d085e75974952e0a4a78af487f3f3
sekong11 <sekong1108@gmail.com>
2025-12-09 14:40:49 +0900

    feat/readingclubreview

---
+++ readingwe-root/readingclub-service/src/main/java/com/ohgiraffers/secondbackend/readingclubreview/client/UserFeignClient.java
@@ -0,0 +1,12 @@
+package com.ohgiraffers.secondbackend.readingclubreview.client;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@FeignClient(name = "user-service")
+public interface UserFeignClient {
+
+    @GetMapping("/")
+
+}
