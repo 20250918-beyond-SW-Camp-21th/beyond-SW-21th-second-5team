74cd883a74c0552c16d77e161167fc7a495da14e
이용우 <qhtm008@naver.com>
2025-12-10 10:15:41 +0900

    test

--- readingwe-root/user-service/src/main/java/com/ohgiraffers/secondbackend/user/controller/AuthController.java
+++ readingwe-root/user-service/src/main/java/com/ohgiraffers/secondbackend/user/controller/AuthController.java
@@ -3,10 +3,7 @@ package com.ohgiraffers.secondbackend.user.controller;
 import com.ohgiraffers.secondbackend.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -16,6 +13,12 @@ import java.util.Map;
 public class AuthController {
 
     private final UserService userService;
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World!";
+    }
+
 
     //회원가입
 
