f69b8e6f84c4c5fc3ae7d03f992e69a72e0080d6
NoSa1e <cjsp1217@gmail.com>
2025-12-10 11:53:58 +0900

    readingclub-security
    readingclub-security

--- readingwe-root/readingclub-service/src/main/java/com/ohgiraffers/readingclubservice/ReadingclubServiceApplication.java
+++ readingwe-root/readingclub-service/src/main/java/com/ohgiraffers/readingclubservice/ReadingclubServiceApplication.java
@@ -5,8 +5,10 @@ import com.ohgiraffers.secondbackend.readingclub.client.UserFeignClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.ohgiraffers.secondbackend"})
 @EnableFeignClients(basePackageClasses = {EmailFeignClient.class, UserFeignClient.class})
 public class ReadingclubServiceApplication {
 
