f63ab75eca0e969e79558cd5823bd7edb060c846
whatimeisitnow <jayjay.hi.77@gmail.com>
2025-12-11 12:26:44 +0900

    testcase bookRerport

--- readingwe-root/book-service/src/main/java/com/ohgiraffers/bookservice/secondbackend/bookreport/entity/BookReportComment.java
+++ readingwe-root/book-service/src/main/java/com/ohgiraffers/bookservice/secondbackend/bookreport/entity/BookReportComment.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -13,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @Entity
 @Table(name = "book_report_comment")
